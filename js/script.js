660a02af0858ff657fd224d7960893e84b7fa497
prmk <priymakvlad420@gmail.com>
2023-10-01 01:01:56 +0300

    v1 header + hero

---
+++ js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav__menu');
+
+hamburger.addEventListener("click", () =>{
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll(".nav__menu-link").forEach(n => n.addEventListener('click', () =>{
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+}))
